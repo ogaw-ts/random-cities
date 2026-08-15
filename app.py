f2a28f2846e50348318361e9ea14b8b2df69f65b
ogaw-ts <13322@metsoc.or.jp>
2026-08-15 18:04:40 +0900

    Enhance UI: Hide header for a cleaner interface

--- app.py
+++ app.py
@@ -48,7 +48,6 @@ st.set_page_config(page_title="おでかけルーレット", page_icon="🎲")
 hide_style = """
     <style>
     .stDeployButton {display:none;}
-    header {visibility: hidden;}
     </style>
 """
 st.markdown(hide_style, unsafe_allow_html=True)
